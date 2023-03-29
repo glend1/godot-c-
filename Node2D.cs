eb6b47cd4fba53013f9f6822b67d98a21212cf0c
glen <glen_dovey@hotmail.com>
2023-03-29 17:27:46 +0100

    first commit

---
+++ Node2D.cs
@@ -0,0 +1,16 @@
+using Godot;
+using System;
+
+public partial class Node2D : Godot.Node2D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+        GD.Print("Hello from C# to Godot :)");
+    }
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
